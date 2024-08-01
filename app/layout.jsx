71b2ec266cfa5af6c3bcca29759e71fcfca65c46
solovyovsdom <joelborofsky@mac.com>
2024-08-01 14:18:16 -0500

    chore: Update font imports, add MainHeading component, adjust styles, and update background and link styles

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,6 +15,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
+      <head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+      </head>
       <body className={`${header.variable} ${body.variable}`}>
         <ThemeProvider>
           {children}
