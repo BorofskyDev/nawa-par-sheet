563c4a186ec1bd9c96bbb36f243cbbd58dcfaf0f
solovyovsdom <joelborofsky@mac.com>
2024-07-31 19:51:55 -0500

    chore: Update font imports and add ThemeProvider to RootLayout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,24 +1,27 @@
-import { righteous, openSans } from "./fonts";
-import LinkContainer from "@/components/layout/link-container/LinkContainer";
-import ThemeToggle from "@/components/layout/theme-toggle/ThemeToggle";
+import { righteous, openSans } from './fonts'
+import LinkContainer from '@/components/layout/link-container/LinkContainer'
+import ThemeToggle from '@/components/layout/theme-toggle/ThemeToggle'
 import '@/styles/globals.scss'
+import { ThemeProvider } from 'next-themes'
 
-
-const header = righteous 
+const header = righteous
 const body = openSans
 
 export const metadata = {
-  title: "Important Sheet",
-  description: "A PAR sheet for expiration dates for items going frozen to ambient",
-};
+  title: 'Important Sheet',
+  description:
+    'A PAR sheet for expiration dates for items going frozen to ambient',
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={`${header.variable} ${body.variable}`}>
-        {children}
-        <LinkContainer />
-        <ThemeToggle />
+        <ThemeProvider>
+          {children}
+          <LinkContainer />
+          <ThemeToggle />
+        </ThemeProvider>
       </body>
     </html>
   )
