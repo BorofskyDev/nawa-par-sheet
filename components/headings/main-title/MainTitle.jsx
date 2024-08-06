ecc16d4fa54268f04e65df90afd15ca681445a57
solovyovsdom <joelborofsky@mac.com>
2024-08-05 19:11:14 -0500

    chore: Update main title and logo components

--- components/headings/main-title/MainTitle.jsx
+++ components/headings/main-title/MainTitle.jsx
@@ -3,11 +3,13 @@ import styles from './MainTitle.module.scss'
 export default function MainTitle({ children }) {
   return (
     <div className={styles.mainTitle}>
-      <h1><span>N</span><span>A</span><span>W</span><span>A</span>
+      <h1>
+        <span>N</span>
+        <span>A</span>
+        <span>W</span>
+        <span>A</span>
       </h1>
-      <p>
-        Not Another Work App
-      </p>
+      <p>Not Another Work App</p>
     </div>
   )
 }
--- components/layout/nav/logo/Logo.jsx
+++ components/layout/nav/logo/Logo.jsx
@@ -3,6 +3,11 @@ import styles from './Logo.module.scss'
 
 export default function Logo(){
     return (
-        <Link href='/' className={styles.logo}>NAWA</Link>
+      <Link href='/' className={styles.logo}>
+        <span>N</span>
+        <span>A</span>
+        <span>W</span>
+        <span>A</span>
+      </Link>
     )
 }
